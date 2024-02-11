08d9eaeaf72442be25749ef53e9e2aeda5aa1d81
Matthis Perrin <matthis.perrin@gmail.com>
2024-02-11 17:02:40 +0100

    Test example code

--- widget/matthis-hass.js
+++ widget/matthis-hass.js
@@ -1,2 +1,42 @@
-(()=>{class t extends HTMLElement{content=null;set hass(t){if(this.content||(this.innerHTML='\n        <ha-card header="Example-card">\n          <div class="card-content"></div>\n        </ha-card>\n      ',this.content=this.querySelector("div")),void 0===this.config?.entity)return;const e=this.config.entity,n=t.states[e],i=n?n.state:"unavailable";this.content&&(this.content.innerHTML=`\n      The state of ${e} is ${i}!\n      <br><br>\n      <img src="http://via.placeholder.com/350x150">\n    `)}setConfig(t){if(void 0===t.entity)throw new Error("You need to define an entity");this.config=t}getCardSize(){return 3}}customElements.define("content-card-example",t)})();
-//# sourceMappingURL=main.237325cad48577e93a75.js.map
+class ContentCardExample extends HTMLElement {
+  // Whenever the state changes, a new `hass` object is set. Use this to
+  // update your content.
+  set hass(hass) {
+    // Initialize the content if it's not there yet.
+    if (!this.content) {
+      this.innerHTML = `
+          <ha-card header="Example-card">
+            <div class="card-content"></div>
+          </ha-card>
+        `;
+      this.content = this.querySelector('div');
+    }
+
+    const entityId = this.config.entity;
+    const state = hass.states[entityId];
+    const stateStr = state ? state.state : 'unavailable';
+
+    this.content.innerHTML = `
+        The state of ${entityId} is ${stateStr}!
+        <br><br>
+        <img src="http://via.placeholder.com/350x150">
+      `;
+  }
+
+  // The user supplied configuration. Throw an exception and Home Assistant
+  // will render an error card.
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    this.config = config;
+  }
+
+  // The height of your card. Home Assistant uses this to automatically
+  // distribute all cards over the available columns.
+  getCardSize() {
+    return 3;
+  }
+}
+
+customElements.define('content-card-example', ContentCardExample);
